42d7857308466459a537d9e972bcb2e01d923cf2
Nathan <nrtheprogrammer@gmail.com>
2015-02-10 20:42:59 -0800

    Added dynamic php pages.

--- service.php
+++ service.php
@@ -24,223 +24,103 @@
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
 	</head>
 	<body class="homepage">
+<?php
 
+	$content = $_GET["content"];
+
+?>
 		<!-- Header -->
 			<header id="header">
 				<div class="logo container">
 					<div>
-						<h1><a href="" id="logo">Services</a></h1>
+						<h1><a href="" id="logo">Services - <?php echo($_GET["content"]); ?></a></h1>
 					</div>
-				</div>
+
 			</header>
 
 		<!-- Nav -->
-			<nav id="nav" class="skel-layers-fixed">
-				<ul>
-					<li><a href="index.html">Home</a></li>
-					<li class="current">
-						<a href="">Services</a>
-						<ul>
-							<li><a href="#">Entertainment / Party</a>
 
-								<ul>
+<nav id="nav" class="skel-layers-fixed">
+	<ul>
+		<li><a href="index.html">Home</a></li>
+		<li class="current">
+			<a href="">Services</a>
+			<ul>
+				<li><a href="#">Entertainment / Party</a>
 
-									<li><a href="#">Invitations</a></li>
-									<li><a href="#">RSVP Coordination</a></li>
-									<li><a href="#">Vendor Coordination</a></li>
-									<li><a href="#">Grocery Shopping</a></li>
-									<li><a href="#">Ticket Procurement</a></li>
+					<ul>
 
-								</ul>
+						<li><a href="#">Invitations</a></li>
+						<li><a href="#">RSVP Coordination</a></li>
+						<li><a href="#">Vendor Coordination</a></li>
+						<li><a href="#">Grocery Shopping</a></li>
+						<li><a href="#">Ticket Procurement</a></li>
 
-							</li>
-							<li><a href="#">Relocation Services</a></li>
-							<li><a href="#">Organizational</a></li>
-							<li><a href="#">Personal Care</a></li>
-							<li><a href="#">Scheduling</a></li>
-							<li><a href="#">Reminder Services</a></li>
-							<li><a href="#">Shopping Services</a></li>
-							<li>
-								<a href="">Phasellus consequat</a>
-								<ul>
-									<li><a href="#">Lorem ipsum dolor</a></li>
-									<li><a href="#">Phasellus consequat</a></li>
-									<li><a href="#">Magna phasellus</a></li>
-									<li><a href="#">Etiam dolore nisl</a></li>
-								</ul>
-							</li>
-							<li><a href="#">Veroeros feugiat</a></li>
-						</ul>
-					</li>
-					<li><a href="#AboutUs">About Us</a></li>
-					<li><a href="#ScheduleServices">Schedule Services</a></li>
-					<li><a href="no-sidebar.html">No Sidebar</a></li>
-				</ul>
-			</nav>
-		
-		<!-- Banner -->
-			<div id="banner-wrapper">
-				<section id="banner">
-					<h2>Welcome to Julies Personal Assistant</h2>
-					<p>Putting the personal in personal assistant.</p>
-					<a href="#" class="button">Learn More</a>
-				</section>
-			</div>
+					</ul>
 
-		<!-- Main -->
-			<div id="main-wrapper">
-				<div id="main" class="container">
-					<div class="row 300%">
-						<div class="12u">
-							
-							<!-- Highlight -->
-
-								<section class="box highlight">
-									<header>
-										<h2>Our services</h2>
-									</header>
-									<p>
-										We offer a very wide range of services,<br/>
-										from entertainment, to relocation services,<br/>
-										to organization and more!<br /></p>
-									<ul class="special">
-										<li><a href="#" class="icon fa-glass"><span class="label"></span></a></li>
-										<p>Entertainment / Party</p>
-										<li><a href="#" class="icon fa-plane"><span class="label"></span></a></li>
-										<p>Relocation Services</p>
-										<li><a href="#" class="icon fa-folder"><span class="label"></span></a></li>
-										<p>Organizational Services</p>
-										<li><a href="#" class="icon fa-calendar"><span class="label"></span></a></li>
-										<p>Scheduling and Planning</p>
-										<li><a href="#" class="icon fa-clock-o"><span class="label"></span></a></li>
-										<p>Reminder Services</p>
-										<li><a href="#Shopping" class="icon fa-shopping-cart"></a></li>
-										<p>Shopping Services</p>
-									</ul>
-
-								</section>
-
-						</div>
-					</div>
-					<div class="row 200%">
-						<div class="12u">
-
-							<!-- Features -->
-								<section class="box features">
-									<h2 class="major"><span>A Major Heading</span></h2>
-									<div>
-										<div class="row">
-											<div class="3u">
-												
-												<!-- Feature -->
-													<section class="box feature">
-														<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-														<h3><a href="#">A Subheading</a></h3>
-														<p>
-															Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-															lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-														</p>
-													</section>
-										
-											</div>
-											<div class="3u">
-												
-												<!-- Feature -->
-													<section class="box feature">
-														<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-														<h3><a href="#">Another Subheading</a></h3>
-														<p>
-															Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-															lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-														</p>
-													</section>
-										
-											</div>
-											<div class="3u">
-												
-												<!-- Feature -->
-													<section class="box feature">
-														<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-														<h3><a href="#">And Another</a></h3>
-														<p>
-															Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-															lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-														</p>
-													</section>
-										
-											</div>
-											<div class="3u">
-												
-												<!-- Feature -->
-													<section class="box feature">
-														<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-														<h3><a href="#">And One More</a></h3>
-														<p>
-															Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-															lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-														</p>
-													</section>
-										
-											</div>
-										</div>
-										<div class="row">
-											<div class="12u">
-												<ul class="actions">
-													<li><a href="#" class="button big">Do Something</a></li>
-													<li><a href="#" class="button alt big">Think About It</a></li>
-												</ul>
-											</div>
-										</div>
-									</div>
-								</section>
-
-						</div>
-					</div>
-				</div>
-			</div>
-
+				</li>
+				<li><a href="#">Relocation Services</a></li>
+				<li><a href="#">Organizational</a></li>
+				<li><a href="#">Personal Care</a></li>
+				<li><a href="#">Scheduling</a></li>
+				<li><a href="#">Reminder Services</a></li>
+				<li><a href="#">Shopping Services</a></li>
+				<li>
+					<a href="">Phasellus consequat</a>
+					<ul>
+						<li><a href="#">Lorem ipsum dolor</a></li>
+						<li><a href="#">Phasellus consequat</a></li>
+						<li><a href="#">Magna phasellus</a></li>
+						<li><a href="#">Etiam dolore nisl</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Veroeros feugiat</a></li>
+			</ul>
+		</li>
+		<li><a href="#AboutUs">About Us</a></li>
+		<li><a href="#ScheduleServices">Schedule Services</a></li>
+	</ul>
+</nav>
 		<!-- Footer -->
-			<footer id="footer" class="container">
-				<div class="row 200%">
-					<div class="12u" >
+<footer id="footer" class="container">
+	<div class="row 200%">
+		<div class="12u" >
 
-						<!-- About -->
-							<section>
-								<h2 class="major"><span>What's this about?</span></h2>
-								<p>
-									This is <strong>TXT</strong>, yet another free responsive site template designed by
-									<a href="http://n33.co">AJ</a> for <a href="http://html5up.net">HTML5 UP</a>. It's released under the
-									<a href="http://html5up.net/license/">Creative Commons Attribution</a> license so feel free to use it for
-									whatever you're working on (personal or commercial), just be sure to give us credit for the design.
-									That's basically it :)
-								</p>
-							</section>
+			<!-- About -->
+			<section>
+				<h2 class="major"><span><?php echo($content); ?></span></h2>
+				<?php
 
-					</div>
-				</div>
-				<div class="row 200%">
-					<div class="12u">
+				echo("<p></p>");
+				?>
 
-						<!-- Contact -->
-							<section>
-								<h2 class="major"><span>Social Media</span></h2>
-								<ul class="contact">
-									<li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
-									<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
-								</ul>
-							</section>
-					
-					</div>
-				</div>
+			</section>
 
-				<!-- Copyright -->
-					<div id="copyright">
-						<ul class="menu">
-							<li>&copy; Julies Personal Assisstant. All rights reserved</li><li>Style by: <a href="http://html5up.net">HTML5 UP</a></li>
-						</ul>
-					</div>
+		</div>
+	</div>
+	<div class="row 200%">
+		<div class="12u">
 
-			</footer>
+			<!-- Contact -->
+			<section>
+				<h2 class="major"><span>Social Media</span></h2>
+				<ul class="contact">
+					<li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
+					<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+				</ul>
+			</section>
+
+		</div>
+	</div>
+
+	<!-- Copyright -->
+	<div id="copyright">
+		<ul class="menu">
+			<li>&copy; Julies Personal Assisstant. All rights reserved</li><li>Style by: <a href="http://html5up.net">HTML5 UP</a></li>
+		</ul>
+	</div>
+
+</footer>
 
 	</body>
 </html>
---
+++ test.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+
+</head>
+<body>
+<form method="get" action="service.php?services=<?php $_GET["content"] ?>">
+
+    <input type="text" name="content">
+    <input type="submit">
+</form>
+</body>
+
+
+
+</html>
