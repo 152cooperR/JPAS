5313f6e88ec1a283474169b1ca7b46eef7ecbd84
Nathan <nrtheprogrammer@gmail.com>
2015-02-24 20:58:14 -0800

    Added password encryption and fixed add user in API

--- api/api.php
+++ api/api.php
@@ -1,28 +1,30 @@
 <?php
-define("key", "KEY");
+define ("ENCRYPTION_KEY", ")(*&^%$$%%^%$%#$%#$^&^&*^");
 function get_mysql(){
 
-    $mysql = new mysqli("localhost","root","root","WebLogins", 8889);
+    $mysql = new mysqli("localhost","root","root","JPAS", 8889);
 
     if($mysql->connect_error){
         die($mysql->connect_error);
+
     }
     return $mysql;
 }
 get_mysql();
 function addUser($username, $password){
 
-    get_mysql()->query("INSERT INTO Users ('Username', 'Password', 'LastLogin') VALUES ($username , $password, TIMESTAMP");
+    get_mysql()->query("INSERT INTO WebLogins (Username, Password) VALUES ('$username', '$password')");
 
 }
 function removeUser($username){
 
-    get_mysql()->query("DELETE FROM Users WHERE Username = $username");
+    get_mysql()->query("DELETE FROM `Users` WHERE Username = '$username''");
 
 }
-function addService($type, $FirstName, $LastName, $state, $date, $id){
+function addService($type, $FirstName, $LastName, $date){
 
-    get_mysql()->query("INSERT INTO Services ('type','FirstName','LastName','state') VALUES ($type, $FirstName, $LastName, $state, $date, $id)");
+    get_mysql()->query("INSERT INTO services (type, FirstName,  LastName, date) VALUES
+ ('$type', '$FirstName', '$LastName', $date)");
 
 }
 
@@ -43,7 +45,7 @@ function changeServiceState($id){
 
     if(get_mysql()->query("")){
 
-        get_mysql()->query("SET state = FALSE WHERE id = $id");
+        get_mysql()->query("SELECT * FROM services WHERE id = $id SET State = FALSE");
 
     }
 
@@ -53,4 +55,24 @@ function getServiceState($id){
 
      return get_mysql()->query("SELECT * FROM services WHERE 'id' = $id")->fetch_assoc();
 
+}
+
+/**
+ * Returns an encrypted & utf8-encoded
+ */
+function encrypt($pure_string, $encryption_key) {
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, $encryption_key, utf8_encode($pure_string), MCRYPT_MODE_ECB, $iv);
+    return $encrypted_string;
+}
+
+/**
+ * Returns decrypted original string
+ */
+function decrypt($encrypted_string, $encryption_key) {
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $encryption_key, $encrypted_string, MCRYPT_MODE_ECB, $iv);
+    return $decrypted_string;
 }
--- test.php
+++ test.php
@@ -1,5 +1,25 @@
-<?php
+<html>
+<head>
+    <?php
 
-require("api/api.php");
-addUser("TEST", "TEST");
+    define("ENCRYPTION_KEY", "!@#$%^&*()");
 
+    require("api/api.php");
+
+    ?>
+</head>
+<body>
+<form action="<?php addService($_POST['type'], $_POST['FirstName'], $_POST['LastName'],
+    $_POST['date']); ?>" method="post">
+
+    <input type="text" name="type" value="<?php $type ?>">
+    <input type="text" name="FirstName" value="<?php $FirstName ?>">
+    <input type="text" name="LastName" value="<?php $LastName ?>">
+    <input type="text" name="date" value="<?php $date ?>">
+    <input type="submit" value="Create user">
+
+
+</form>
+
+</body>
+</html>
