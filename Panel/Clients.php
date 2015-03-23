f06f636e25270262cba425aba5d632e4a01735b4
152cooperR <nrtheprogrammer@gmail.com>
2015-03-22 22:15:11 -0700

    Added bootstrap and client panel pages, as well as htaccess

---
+++ Panel/Clients.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css"/>
+
+</head>
+<body>
+
+    <div class="jumbotron">
+
+        <div class="container">
+
+            <h1>Clients</h1>
+
+        </div>
+
+    </div>
+
+<div class="container">
+
+    <table class="table table-bordered">
+
+        <tr>
+
+            <td>ID</td>
+            <td>Client Name</td>
+            <td>Order Status</td>
+
+        </tr>
+        <tr>
+
+            <td><a>1</a></td>
+            <td>Nathan Rhodes</td>
+            <td><button class="btn btn-danger">Not Complete</button></td>
+
+        </tr>
+        <tr>
+
+            <td><a>2</a></td>
+            <td>Nathan Rhodes</td>
+            <td><button class="btn btn-success">Complete</button></td>
+
+        </tr>
+
+
+    </table>
+
+</div>
+
+</body>
+
+<script src="js/jquery-2.1.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</html>
